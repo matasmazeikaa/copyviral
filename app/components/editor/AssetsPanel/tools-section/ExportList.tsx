944318df376d1b786b1e3a6d3f4ce09649be9881
Matas Mazeika <matasmazeika@gmail.com>
2026-01-19 17:00:07 -0300

    remove unused component

--- app/components/editor/AssetsPanel/tools-section/ExportList.tsx
+++
@@ -1,8 +0,0 @@
-import Export from "../../render/Ffmpeg/Export";
-export default function ExportList() {
-    return (
-        <div className="flex flex-col gap-4">
-            <Export />
-        </div>
-    )
-}
