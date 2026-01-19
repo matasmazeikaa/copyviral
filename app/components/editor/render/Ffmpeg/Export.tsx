64640fa606906a27f13fc59c87ad3405cafdfa63
Matas Mazeika <matasmazeika@gmail.com>
2026-01-19 16:59:04 -0300

    remove unused component

--- app/components/editor/render/Ffmpeg/Export.tsx
+++
@@ -1,8 +0,0 @@
-import Ffmpeg from "./Ffmpeg";
-export default function ExportList() {
-    return (
-        <div className="flex flex-col justify-center items-center h-full w-full">
-            <Ffmpeg />
-        </div>
-    )
-}
